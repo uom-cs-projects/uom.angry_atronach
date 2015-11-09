6be527a69ef06d9f1f43bb70483dbcd9b7c4564f
Mathew Campbell <curiousinternals@gmail.com>
2015-11-09 05:12:33 +0000

    Resources can now be edited: Resolves #17.

--- src/main/scala/com/curiousinternals/typ/TYPServlet.scala
+++ src/main/scala/com/curiousinternals/typ/TYPServlet.scala
@@ -8,6 +8,42 @@ class TYPServlet extends ThirdYearProjectStack
     with ScalateSupport
     with DatabaseSessionSupport {
 
+  get("/resource/update/:id") {
+
+    val id: Long = {
+      try { params("id").toLong }
+      catch { case e: Exception => halt(400) }
+    }
+
+    var resource : Option[Resource] = {
+      transaction {
+        TYPDB.resources.where(r => r.id === id).headOption
+  	  }
+    }
+
+    resource match {
+      case Some(resource) =>
+        contentType="text/html"
+        jade("resource/update",
+            "title" -> "Update Resource", "resource_data" -> resource.content)
+
+      case None => NotFound(s"Resource '${id}' was not found.")
+    }
+
+  }
+
+  /*post("/resource/update/:id") {
+
+    val content: String = params.getOrElse("content", halt(400))
+
+    val id = {
+      transaction { TYPDB.resources.insert(new Resource(0, content)) }.id
+    }
+
+    redirect(s"/resource/read/${id}")
+
+  }*/
+
   get("/resource/delete/:id") {
 
     val id: Long = {
@@ -25,8 +61,6 @@ class TYPServlet extends ThirdYearProjectStack
 
   get("/resource/read/:id") {
 
-    contentType="text/html"
-
     val id: Long = {
       try { params("id").toLong }
       catch { case e: Exception => halt(400) }
@@ -40,6 +74,7 @@ class TYPServlet extends ThirdYearProjectStack
 
     resource match {
       case Some(resource) =>
+        contentType="text/html"
         jade("resource/read",
             "title" -> "Resource Viewer",
             "resource_data" -> resource.content)
