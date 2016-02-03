a41257c729cc029c92bf77ca883edc83fd6eda73
Mathew Campbell <curiousinternals@gmail.com>
2016-02-03 17:17:45 +0000

    Removed default play example in favour of heroku example.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,34 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.cache.Cache
+import play.api.Play.current
 
-class Application extends Controller {
+import play.api.db._
+
+object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(null))
   }
 
+  def db = Action {
+    var out = ""
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement
+
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+
+      val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+      while (rs.next) {
+        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(out)
+  }
 }
--- build.sbt
+++ build.sbt
@@ -1,20 +1,16 @@
-name := """angry-atronach"""
+name := """play-getting-started"""
 
 version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
   jdbc,
   cache,
-  ws,
-  specs2 % Test
+  "org.postgresql" % "postgresql" % "9.4-1201-jdbc41",
+  ws
 )
 
-resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
-
-// Play provides two styles of routers, one expects its actions to be injected, the
-// other, legacy style, accesses its actions statically.
-routesGenerator := InjectedRoutesGenerator
+libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _ )
