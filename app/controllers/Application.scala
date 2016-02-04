981aa9dd1b006a22d22b7af6cd40ceba69441596
Mathew Campbell <curiousinternals@gmail.com>
2016-02-04 17:09:50 +0000

    Basic UI design.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,6 +13,10 @@ object Application extends Controller {
     Ok(views.html.index())
   }
 
+  def resource(id: String) = Action {
+    Ok(views.html.resource.read(id.toLong))
+  }
+
   def db = Action {
     var out = ""
     val conn = DB.getConnection()
