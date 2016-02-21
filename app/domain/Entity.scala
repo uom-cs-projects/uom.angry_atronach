f3f6a7db5184cb67a41702eb1255a31f340a8243
Mathew Campbell <curiousinternals@gmail.com>
2016-02-21 14:33:05 +0000

    Entity now has equals and hashcode methods based on the neo4j-ogm tutorial.

--- app/domain/Entity.scala
+++ app/domain/Entity.scala
@@ -5,6 +5,16 @@ import org.neo4j.ogm.annotation.GraphId
 abstract class Entity {
 
   @GraphId
-  var internalId: java.lang.Long = _
+  var graphId: java.lang.Long = _
+
+  override def equals(o: Any): Boolean = o match {
+    case other: Entity => other.graphId.equals(this.graphId)
+    case _ => false
+  }
+
+  override def hashCode: Int = graphId match {
+    case id: java.lang.Long => graphId.hashCode()
+    case _ => -1
+  }
 
 }
