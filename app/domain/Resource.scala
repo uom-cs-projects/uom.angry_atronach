c9f101f378a988ee0816c9b923594db2009d1ae1
Mathew Campbell <curiousinternals@gmail.com>
2016-02-21 14:54:09 +0000

    Added no-args constructors for all the domain entities as required by neo4j-ogm

--- app/domain/Resource.scala
+++ app/domain/Resource.scala
@@ -5,8 +5,11 @@ import java.util.UUID
 import org.neo4j.ogm.annotation.{NodeEntity, Relationship}
 
 @NodeEntity
-class Resource(var title: String, var content: String) extends Entity {
+class Resource() extends Entity {
   val uuid: String = UUID.randomUUID.toString
+
+  var title: String = _
+  var content: String = _
 
   @Relationship(`type` = "DEPENDS_ON", direction = Relationship.OUTGOING)
   var prerequisites: Set[DependsOn] = Set()
@@ -16,4 +19,10 @@ class Resource(var title: String, var content: String) extends Entity {
 
   @Relationship(`type` = "UNDERSTANDS", direction = Relationship.INCOMING)
   var understoodBy: Set[Understands] = Set()
+
+  def this(title: String, content: String) {
+    this
+    this.title = title
+    this.content = content
+  }
 }
--- app/domain/User.scala
+++ app/domain/User.scala
@@ -3,7 +3,15 @@ package domain
 import org.neo4j.ogm.annotation.{NodeEntity, Relationship}
 
 @NodeEntity
-class User(var username: String) extends Entity {
+class User() extends Entity {
+
+  var username: String = _
+
   @Relationship(`type` = "UNDERSTANDS", direction = Relationship.OUTGOING)
   var understands: Set[Understands] = Set()
+
+  def this(username: String) {
+    this
+    this.username = username
+  }
 }
