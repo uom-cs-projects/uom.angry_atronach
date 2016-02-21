d8a86f3b66e1c079293e1f0ea9f18bff7de879ee
Mathew Campbell <curiousinternals@gmail.com>
2016-02-21 13:44:17 +0000

    Relocated resource related urls to a Resource controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,22 +17,6 @@ object Application extends Controller {
     Ok(views.html.index())
   }
 
-  def resource(id: String) = Action {
-    val session = Neo4jSessionFactory.getNeo4jSession()
-    val query =
-        "MATCH (r:Resource) WHERE r.uuid = {uuid} RETURN r.title, r.content";
-    var parameters = new java.util.HashMap[String, String]()
-    parameters.put("uuid", id)
-
-    val result = session.query(query, parameters)
-    val test = result.queryResults
-    val resource = result.queryResults.iterator.next()
-    val title = (resource get "r.title").asInstanceOf[String]
-    val content = (resource get "r.content").asInstanceOf[String]
-
-    Ok(views.html.resource.read(id, title, content))
-  }
-
   def db = Action {
     val user = new User("curiousinternals")
 
